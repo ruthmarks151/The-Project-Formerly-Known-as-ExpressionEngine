1e0285dbe635dd17b2aba5b4406b087e624d17a5
Ryan Marks <Ryan@Sigmabot.com>
2015-12-02 18:48:27 -0500

    Initial version of expression engine. Does not work

---
+++ main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#define NO_OP_FOUND -1
+using namespace std;
+
+bool isOp(char c){
+    return c == '+' || c == '-' || c == '*' || c == '/';
+}
+
+void splitExpression(string expression){
+    int parenDepth = 0;
+    int firstLowPresedenceOperator = NO_OP_FOUND;
+    int firstHighPresedenceOperator = NO_OP_FOUND;
+    char cbefore = expression[0];
+    for (int i = 1; i < expression.length() &&  (firstLowPresedenceOperator == NO_OP_FOUND); i++) {
+        char c = expression[i];
+        switch (c) {
+            case '(':
+                parenDepth++;
+                break;
+            case ')':
+                parenDepth--;
+                break;
+            case '-':
+            case '+':
+                if (!(cbefore == '(' || isOp(cbefore))) {
+                    if (parenDepth == 0) {
+                        firstLowPresedenceOperator = i;
+                    }
+                }
+                break;
+            case '*':
+            case '/':
+                if (firstHighPresedenceOperator == NO_OP_FOUND) {
+                    if (parenDepth == 0) {
+                        firstHighPresedenceOperator = i;
+                    }
+                }
+                break;
+        }
+        cbefore = c;
+    }
+        int splitPoint = firstLowPresedenceOperator;
+        if (firstLowPresedenceOperator == NO_OP_FOUND)
+            splitPoint = firstHighPresedenceOperator;
+
+        string left = expression.substr(0,splitPoint);
+        string right = expression.substr(splitPoint+1,expression.length());
+        char op = expression[splitPoint];
+
+        cout << '"' << left << '"' << op << '"' << right << '"' << endl;
+
+}
+
+int main() {
+    string expression;
+    cout << "Hello, World!" << endl;
+    while (true) {
+        getline(cin, expression);
+        splitExpression(expression);
+    }
+    return 0;
+}
