ce45ed92942516457088d4245030229a338c758a
lyhds18 <Sophia@Sophias-MacBook-Pro.local>
2015-12-03 15:01:07 -0500

    added comments

--- Subtraction.cpp
+++ Subtraction.cpp
@@ -1,24 +1,24 @@
 #include "Subtraction.h"
 #include <iostream>
 
-Subtraction::Subtraction(){
-    left = nullptr;
-    right = nullptr;
+Subtraction::Subtraction(){ //Default constructor, should not be used
+    left = nullptr; // Making the left a null pointer
+    right = nullptr; // Making the right a null pointer
 };
 
 
-Subtraction::Subtraction(Expression *givenLeft, Expression *givenRight) : ArithmeticExpression(givenLeft,givenRight){
+Subtraction::Subtraction(Expression *givenLeft, Expression *givenRight) : ArithmeticExpression(givenLeft,givenRight){ // addition taking in parameters
 
 }
 
 std::string Subtraction::evaluate(){
-    return std::to_string(std::stof(left->evaluate().c_str()) - std::stof(right->evaluate().c_str()));
+    return std::to_string(std::stof(left->evaluate().c_str()) - std::stof(right->evaluate().c_str())); //takes in the values, performs subtraction and turns then answer from a double to a string
 }
 
-float Subtraction::convert(std::string s){
+float Subtraction::convert(std::string s){ //converts string into a float
     return 0;
 }
 
 void Subtraction::print(){
-    std::cout << "This is an arithmetic expression(subtraction)!" << std::endl;
+    std::cout << "This is an arithmetic expression(subtraction)!" << std::endl; //used in testing
 }
