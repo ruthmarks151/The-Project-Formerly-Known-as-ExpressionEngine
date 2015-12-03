c9a9bedffe362b0fdd9bdbc4c1875ee974e08e7b
Nishanth Balamohan <balamon@mcmaster.ca>
2015-12-02 22:35:16 -0500

    Added comments to addition and division .cpp

--- Addition.cpp
+++ Addition.cpp
@@ -2,24 +2,24 @@
 #include <iostream>
 #include <string>
 
-Addition::Addition(){
-    left = nullptr;
-    right = nullptr;
+Addition::Addition(){//Default constructor, should not be used
+    left = nullptr;// Making the left a null pointer
+    right = nullptr;// Making the right a null pointer
 };
 
 
-Addition::Addition(Expression *givenLeft, Expression *givenRight) : ArithmeticExpression(givenLeft,givenRight) {
+Addition::Addition(Expression *givenLeft, Expression *givenRight) : ArithmeticExpression(givenLeft,givenRight) {//Addition taking in parameters
 
 }
 
 std::string Addition::evaluate(){
-    return std::to_string(std::stof(left->evaluate().c_str()) + std::stof(right->evaluate().c_str()));
+    return std::to_string(std::stof(left->evaluate().c_str()) + std::stof(right->evaluate().c_str()));//Takes in the values and adds them while converting and returning from a double to a string
 }
 
-float Addition::convert(std::string s){
+float Addition::convert(std::string s){//Converts the string into a float
     return 0;
 }
 
 void Addition::print(){
-    std::cout << "This is an arithmetic expression(addition)!" << std::endl;
+    std::cout << "This is an arithmetic expression(addition)!" << std::endl;//Used in testing
 }
--- Division.cpp
+++ Division.cpp
@@ -2,24 +2,24 @@
 #include <iostream>
 #include <string>
 
-Division::Division(){
-    left = nullptr;
-    right = nullptr;
+Division::Division(){//The default constructor which should not be used
+    left = nullptr;//Making the left pointer null
+    right = nullptr;//Making the right pointer null
 };
 
 
-Division::Division(Expression *givenLeft, Expression *givenRight) : ArithmeticExpression(givenLeft,givenRight) {
+Division::Division(Expression *givenLeft, Expression *givenRight) : ArithmeticExpression(givenLeft,givenRight) {//Division taking in parameters
 
 }
 
 std::string Division::evaluate() {
-    return std::to_string(std::stof(left->evaluate().c_str()) / std::stof(right->evaluate().c_str()));
+    return std::to_string(std::stof(left->evaluate().c_str()) / std::stof(right->evaluate().c_str()));//Divides the values while converting and returning a string
 }
 
-float Division::convert(std::string s) {
+float Division::convert(std::string s) {//Converts from a string to a float
     return 0;
 }
 
 void Division::print(){
-    std::cout<<"This is an Arithmetic Expression(division)" << std::endl;
+    std::cout<<"This is an Arithmetic Expression(division)" << std::endl;//Used in testing
 }
